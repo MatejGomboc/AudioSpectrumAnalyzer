01ac89f8186fdfe1a99e66323312c80441240545
Matej Gomboc <matejg03@gmail.com>
2021-05-02 10:31:08 +0200

    Update.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include <QFile>
 
 int main(int argc, char* argv[])
 {
     QApplication application(argc, argv);
-
     application.setWindowIcon(QIcon(":/icon.svg"));
 
     MainWindow main_window;
